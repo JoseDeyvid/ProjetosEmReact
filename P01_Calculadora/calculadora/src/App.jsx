586863dfad70fac4ba24537552caa8707370d2c8
Jose Deyvid <j_deyvid@live.com>
2024-09-03 18:03:42 -0300

    Iniciar projeto

---
+++ P01_Calculadora/calculadora/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <div className="content">
+        <input type="text" />
+        <input type="text" />
+        <button>AC</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
