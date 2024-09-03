060295588acb46afa386f9a53a844a4a5b218864
Jose Deyvid <j_deyvid@live.com>
2024-09-03 18:50:00 -0300

    Finalizar essencial do css dos botoes

--- P01_Calculadora/calculadora/src/App.jsx
+++ P01_Calculadora/calculadora/src/App.jsx
@@ -1,14 +1,40 @@
 import { useState } from 'react'
 import './App.css'
+import Button from './components/Button'
 
 function App() {
+
+  const [equation, setEquation] = useState('0')
+
+  const insertNumberOnEquation = (n) => {
+    setEquation((prevEquation) => prevEquation + n);
+  }
 
   return (
     <div className='container'>
       <div className="content">
-        <input type="text" />
-        <input type="text" />
-        <button>AC</button>
+        <input type="text" className='input-result' disabled />
+        <div className='input-equation'>
+          <input type="text" value={equation} disabled />
+        </div>
+        <div className="calc-keyboard">
+          <Button value={'AC'} handleClick={() => insertNumberOnEquation(2)} />
+          <Button value={'AC'} />
+          <Button value={'AC'} />
+          <Button value={'AC'} />
+          <Button value={'AC'} />
+          <Button value={'AC'} />
+          <Button value={'AC'} />
+          <Button value={'AC'} />
+          <Button value={'AC'} />
+          <Button value={'AC'} />
+          <Button value={'AC'} />
+          <Button value={'AC'} />
+          <Button value={'AC'} />
+          <Button value={'AC'} />
+          <Button value={'AC'} />
+          <Button value={'AC'} />
+        </div>
       </div>
     </div>
   )
