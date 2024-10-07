8851c791a18809f731919c3dc39f584f9ed77642
Jose Deyvid <j_deyvid@live.com>
2024-10-06 22:27:39 -0300

    Criar html do projeto

--- P02_Cronometro/cronometro/src/App.jsx
+++ P02_Cronometro/cronometro/src/App.jsx
@@ -1,10 +1,38 @@
+import { useState } from "react"
 import "./App.css"
+import Button from "./components/Button"
 
 function App() {
 
+  const [timerIsActive, setTimerIsActive] = useState(true);
+
   return (
-    <div>
-      
+    <div className="container">
+      <div className="content">
+        <label className="label-timer">00:00:00</label>
+        {timerIsActive ?
+          <div>
+            <Button title="Parar" />
+            <Button title="Volta" />
+            <Button title="Zerar" />
+          </div>
+          :
+          <div>
+            <Button title="Iniciar" />
+            <Button title="Zerar" />
+          </div>}
+
+        <label>Voltas:</label>
+        {timerIsActive &&
+          <div>
+            <label>Volta 1: 00:02:42</label>
+            <label>Volta 2: 00:04:42</label>
+            <label>Volta 3: 01:07:42</label>
+          </div>
+
+        }
+
+      </div>
     </div>
   )
 }
