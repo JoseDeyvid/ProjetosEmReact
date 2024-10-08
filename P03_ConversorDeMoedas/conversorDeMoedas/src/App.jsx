24d4b802d31f51396fb017811a89b2dc85395f92
Jose Deyvid <j_deyvid@live.com>
2024-10-08 19:40:13 -0300

    Fazer funcionar da maneira correta o ultimo select apenas

--- P03_ConversorDeMoedas/conversorDeMoedas/src/App.jsx
+++ P03_ConversorDeMoedas/conversorDeMoedas/src/App.jsx
@@ -5,23 +5,39 @@ import axios from "axios";
 function App() {
 
   const [options, setOptions] = useState([]);
+  const [valueToConvert, setValueToConvert] = useState(1);
+  const [valueConverted, setValueConverted] = useState(1);
+  const [currencyToConvert, setCurrencyToConvert] = useState("USD");
+  const [currencyConverted, setCurrencyConverted] = useState("USD");
+
+  const handleChangeCurrencyToConvert = () => {
+
+  }
+
+  const handleChangeCurrencyConverted = (e) => {
+    setCurrencyConverted(e.target.value);
+    
+    const newValue = valueToConvert * options[e.target.value]
+    setValueConverted(newValue);
+  }
 
   useEffect(() => {
-    axios.get("https://v6.exchangerate-api.com/v6/cec3c9f9369a5769492c8c92/latest/USD").then((response) => {
-      const currencies = Object.keys(response.data.conversion_rates);
-      setOptions(currencies);
+    console.log("Teste")
+    axios.get(`https://v6.exchangerate-api.com/v6/cec3c9f9369a5769492c8c92/latest/${currencyToConvert}`).then((response) => {
+      // const currencies = Object.keys();
+      setOptions(response.data.conversion_rates);
     })
-  }, [])
+  }, [currencyToConvert])
 
   return (
     <div className="container">
       <div className="content">
         <h1>Conversor de moedas</h1>
-        <input type="number" />
-        <SelectCurrency title={"Selecione a moeda:"} options={options} />
-        <SelectCurrency title={"para"} options={options} />
-        <h2>0.94 EUR</h2>
-        <p>1 USD valem 0.94 EUR</p>
+        <input type="number" value={valueToConvert} onChange={(e) => setValueToConvert(e.target.value)} />
+        <SelectCurrency title={"Selecione a moeda:"} options={options} optionSelected={currencyToConvert} handleChangeOption={handleChangeCurrencyConverted} />
+        <SelectCurrency title={"para"} options={options} optionSelected={currencyConverted} handleChangeOption={handleChangeCurrencyConverted} />
+        <h2>{valueToConvert} {currencyToConvert}</h2>
+        <p>{valueToConvert} {currencyToConvert} valem {valueConverted} {currencyConverted}</p>
       </div>
     </div>
   )
--- P03_ConversorDeMoedas/conversorDeMoedas/src/components/SelectCurrency/index.jsx
+++ P03_ConversorDeMoedas/conversorDeMoedas/src/components/SelectCurrency/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import "./styles.css"
 
-const SelectCurrency = ({title, options}) => {
+const SelectCurrency = ({title, options, optionSelected, handleChangeOption}) => {
     return (
         <div className="select-group">
             <label htmlFor="currency-converted">{title}</label>
-            <select name="currency-converted" id="currency-converted">
-                {options.map((option, i) => (
+            <select name="currency-converted" id="currency-converted" value={optionSelected} onChange={handleChangeOption}>
+                {Object.keys(options).map((option, i) => (
                     <option value={option} key={i}>{option}</option>
                 ))}
             </select>
