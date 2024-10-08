0961f75a814ce9972a0de14b0e416eaa1fca08ea
Jose Deyvid <j_deyvid@live.com>
2024-10-08 18:40:49 -0300

    Adicionar css do projeto

--- P03_ConversorDeMoedas/conversorDeMoedas/src/App.jsx
+++ P03_ConversorDeMoedas/conversorDeMoedas/src/App.jsx
@@ -10,8 +10,8 @@ function App() {
       <div className="content">
         <h1>Conversor de moedas</h1>
         <input type="number" />
-        <SelectCurrency title={"Selecione a moeda:"} options={options}/>
-        <SelectCurrency title={"para"} options={options}/>
+        <SelectCurrency title={"Selecione a moeda:"} options={options} />
+        <SelectCurrency title={"para"} options={options} />
         <h2>0.94 EUR</h2>
         <p>1 USD valem 0.94 EUR</p>
       </div>
--- P03_ConversorDeMoedas/conversorDeMoedas/src/components/SelectCurrency/index.jsx
+++ P03_ConversorDeMoedas/conversorDeMoedas/src/components/SelectCurrency/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import "./styles.css"
 
 const SelectCurrency = ({title, options}) => {
     return (
-        <div className="input-group">
-            <label htmlFor="currency-converted">para</label>
+        <div className="select-group">
+            <label htmlFor="currency-converted">{title}</label>
             <select name="currency-converted" id="currency-converted">
                 {options.map((option) => (
                     <option value={option}>{option}</option>
