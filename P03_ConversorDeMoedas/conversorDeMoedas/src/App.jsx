e80807f2067e7eff41659c6597171b7486ce08ef
Jose Deyvid <j_deyvid@live.com>
2024-10-08 18:06:13 -0300

    Finalizar html do projeto

--- P03_ConversorDeMoedas/conversorDeMoedas/src/App.jsx
+++ P03_ConversorDeMoedas/conversorDeMoedas/src/App.jsx
@@ -3,8 +3,29 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-      
+    <div className="container">
+      <div className="content">
+        <h1>Conversor de moedas</h1>
+        <input type="number" />
+        <div className="input-group">
+          <label htmlFor="currency-to-convert">Selecione a moeda:</label>
+          <select name="currency-to-convert" id="currency-to-convert">
+            <option value="USD">USD</option>
+            <option value="BRL">BRL</option>
+            <option value="EUR">EUR</option>
+          </select>
+        </div>
+        <div className="input-group">
+          <label htmlFor="currency-converted">para</label>
+          <select name="currency-converted" id="currency-converted">
+            <option value="USD">USD</option>
+            <option value="BRL">BRL</option>
+            <option value="EUR">EUR</option>
+          </select>
+        </div>
+        <h2>0.94 EUR</h2>
+        <p>1 USD valem 0.94 EUR</p>
+      </div>
     </div>
   )
 }
