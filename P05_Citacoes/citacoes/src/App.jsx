994aa83a8449b63e7e9a0fc60bb5628b7d2eff41
Jose Deyvid <j_deyvid@live.com>
2024-10-15 10:19:08 -0300

    Adicionar todos os elementos na tela

--- P05_Citacoes/citacoes/src/App.jsx
+++ P05_Citacoes/citacoes/src/App.jsx
@@ -1,11 +1,17 @@
+import { useState } from 'react'
 import './App.css'
+import data from "./data"
+import Quote from './components/Quote'
 
 function App() {
-
+  const [quote, setQuote] = useState(data[0])
   return (
-    <>
-      <h1>TESTE</h1>
-    </>
+    <div>
+      <Quote quote={quote} />
+      <button>Traduzir para o Inglês</button>
+      <button>Traduzir para o Espanhol</button>
+      <button>Próxima citação</button>
+    </div>
   )
 }
 
