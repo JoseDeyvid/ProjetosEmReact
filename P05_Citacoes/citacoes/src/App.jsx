74ca435ec9914b756095b649ba067c402b25df83
Jose Deyvid <j_deyvid@live.com>
2024-10-15 11:29:49 -0300

    Finalizar o projeto, sem as funcoes de traducao porque a api usada passou a ser paga

--- P05_Citacoes/citacoes/src/App.jsx
+++ P05_Citacoes/citacoes/src/App.jsx
@@ -4,15 +4,37 @@ import data from "./data"
 import Quote from './components/Quote'
 
 function App() {
-  const[quoteId, setQuoteId] = useState(0);
+  const [quoteId, setQuoteId] = useState(0);
   const [quote, setQuote] = useState(data[quoteId]);
+  const [language, setLanguage] = useState("pt_br")
 
   const nextQuote = () => {
-    if(quoteId === data.length-1) {
+    if (quoteId === data.length - 1) {
       setQuoteId(0);
     } else {
-      setQuoteId((prevQuoteId) => prevQuoteId+1);
+      setQuoteId((prevQuoteId) => prevQuoteId + 1);
     }
+  }
+
+  // Não foi possível implementar as funções de tradução pois a API utilizada no curso passou a ser paga.
+  const translateToEn = () => {
+    // const url = `https://libretranslate.com/translate?q=${quote.texto}?source=pt?target=en`;
+    // try {
+    //   const response = await fetch(url, {
+    //     method: "POST"
+    //   });
+    //   if (!response.ok) {
+    //     throw new Error(`Response status: ${response.status}`);
+    //   }
+
+    //   const json = await response.json();
+    //   console.log(json);
+    // } catch (error) {
+    //   console.error(error.message);
+    // }
+    setQuote({
+      texto: "Não foi possível implementar as funções de tradução pois a API utilizada no curso passou a ser paga."
+    });
   }
 
   useEffect(() => {
@@ -21,9 +43,9 @@ function App() {
 
   return (
     <div className='container pt-4'>
-      <Quote quote={quote} />
-      <button className='btn btn-primary me-2'>Traduzir para o Inglês</button>
-      <button className='btn btn-secondary'>Traduzir para o Espanhol</button>
+      <Quote quote={quote} language={language} />
+      <button className='btn btn-primary me-2' onClick={translateToEn}>Traduzir para o Inglês</button>
+      <button className='btn btn-secondary' onClick={translateToEn}>Traduzir para o Espanhol</button>
       <button className='btn btn-success d-block mt-2' onClick={nextQuote}>Próxima citação</button>
     </div>
   )
--- P05_Citacoes/citacoes/src/components/Quote/index.jsx
+++ P05_Citacoes/citacoes/src/components/Quote/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Quote = ({quote}) => {
+const Quote = ({quote, language}) => {
   return (
     <div className='d-flex flex-column'>
       <p className='m-0'>{quote.texto}</p>
