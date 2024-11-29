b06797cc11484493097296b2bf5b0f0e2c5e76d9
Jose Deyvid <j_deyvid@live.com>
2024-11-28 22:51:48 -0300

    Montar estrutura da pagina de NewPost

--- P18_BlogComAxios/blogComAxios/src/pages/NewPost/NewPost.jsx
+++ P18_BlogComAxios/blogComAxios/src/pages/NewPost/NewPost.jsx
@@ -4,6 +4,15 @@ const NewPost = () => {
   return (
     <div>
       <h2>Inserir novo post: </h2>
+      <div className="formControl">
+        <label htmlFor="postTitle">Título:</label>
+        <input type="text" id='postTitle' name='postTitle' placeholder='Digite o título...' />
+      </div>
+      <div className="formControl">
+        <label htmlFor="postBody">Descrição:</label>
+        <input type="text" id='postBody' name='postBody' placeholder='Digite o conteúdo...' />
+      </div>
+      <button>Criar post</button>
     </div>
   )
 }
