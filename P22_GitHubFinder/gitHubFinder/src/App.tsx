4f4b93e6c815fd264260de70aae16529d649f939
Jose Deyvid <j_deyvid@live.com>
2024-12-18 22:18:46 -0300

    Initial commit

---
+++ P22_GitHubFinder/gitHubFinder/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>GitHub Finder</h1>
+      <form>
+        <h2>Busque por um usuário:</h2>
+        <p>Conheça seus melhores repositórios</p>
+        <input type="text" placeholder="Digite o nome do usuário" />
+      </form>
+    </div>
+  );
+}
+
+export default App;
