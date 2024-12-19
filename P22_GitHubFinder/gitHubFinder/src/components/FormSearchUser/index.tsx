55ad94f20028388e896fa6f1a86f47bf59a29940
Jose Deyvid <j_deyvid@live.com>
2024-12-19 11:01:39 -0300

    Buscar e salvar dados do user pesquisado no state

--- P22_GitHubFinder/gitHubFinder/src/components/FormSearchUser/index.tsx
+++ P22_GitHubFinder/gitHubFinder/src/components/FormSearchUser/index.tsx
@@ -1,12 +1,32 @@
-import React from "react";
+import { FormEvent, useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import { useUser } from "../../contexts/UserContext";
+import { User } from "../../utils/type";
 
 const FormSearchUser = () => {
+  const [userName, setUserName] = useState("");
+  const { setUser } = useUser();
+
+  const handleSearchUser = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(`https://api.github.com/users/${userName}`);
+      const userJson = (await res.json()) as User;
+      setUser(userJson);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <form>
+    <form onSubmit={(e) => handleSearchUser(e)}>
       <h2>Busque por um usuário:</h2>
       <p>Conheça seus melhores repositórios</p>
-      <input type="text" placeholder="Digite o nome do usuário" />
+      <input
+        type="text"
+        placeholder="Digite o nome do usuário"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
       <button type="submit">
         <FaSearch />
       </button>
--- P22_GitHubFinder/gitHubFinder/src/components/UserInfos/index.tsx
+++ P22_GitHubFinder/gitHubFinder/src/components/UserInfos/index.tsx
@@ -1,5 +1,12 @@
+import { useUser } from "../../contexts/UserContext";
+
 const UserInfos = () => {
-  return <div>UserInfos</div>;
+  const { user } = useUser();
+  return (
+    <div>
+      <img src={user?.avatar_url} alt={`foto de perfil do ${user?.name}`} />
+    </div>
+  );
 };
 
 export default UserInfos;
