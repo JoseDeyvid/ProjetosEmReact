1678533f7df44bca23be0dbc7e4e1b1a0e83ec46
Jose Deyvid <j_deyvid@live.com>
2025-01-16 11:20:52 -0300

    Testar validacoes e iniciar css

--- P23_FormComTS/formWithTS/src/App.tsx
+++ P23_FormComTS/formWithTS/src/App.tsx
@@ -1,7 +1,10 @@
+import Form from "./components/Form";
+
 function App() {
   return (
-    <div>
+    <div className="bg-zinc-100 min-h-screen w-full flex flex-col items-center gap-6 pt-8">
       <h1 className="text-3xl font-bold underline">Form com TS</h1>
+      <Form />
     </div>
   );
 }
