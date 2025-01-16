e3243f17904a6c855987251bdafae6d96a5e9c45
Jose Deyvid <j_deyvid@live.com>
2025-01-16 10:29:36 -0300

    Configurar schema com zod

---
+++ P23_FormComTS/formWithTS/src/schemas/formSchema.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+const formSchema = z
+  .object({
+    name: z.string().min(3, "Campo deve possuir pelo menos 3 caracteres."),
+    lastname: z.string().min(3, "Campo deve possuir pelo menos 3 caracteres."),
+    gender: z.string().refine((field) => field !== "select", {
+      message: "Selecione um Gênero.",
+    }),
+    email: z
+      .string()
+      .min(1, "Esse campo é obrigatório!")
+      .email("Email inválido!"),
+    password: z.string().min(6, "Senha deve ter pelo menos 6 caracteres."),
+    confirmpassword: z
+      .string()
+      .min(6, "Senha deve ter pelo menos 6 caracteres."),
+    agree: z.boolean().refine((field) => field === true, {
+      message: "Você precisa concordar com os termos.",
+    }),
+  })
+  .refine((field) => field.password === field.confirmpassword, {
+    message: "Senhas não coincidem.",
+    path: ["confirm"],
+  });
+
+export type FormSchem = z.infer<typeof formSchema>;
