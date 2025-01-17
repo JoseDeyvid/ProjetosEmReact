443cca427954ee6af1006b01b74f8e0b22274101
Jose Deyvid <j_deyvid@live.com>
2025-01-17 19:11:42 -0300

    Criar cards e funcao de embaralhar

--- P24_JogoDaMemoria/jogoDaMemoria/src/App.tsx
+++ P24_JogoDaMemoria/jogoDaMemoria/src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Game from "./components/Game";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Game />
+    </div>
+  );
 }
 
 export default App;
---
+++ P24_JogoDaMemoria/jogoDaMemoria/src/components/Board.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "./Card";
+
+const Board = () => {
+  return (
+    <div>
+      <Card />
+    </div>
+  );
+};
+
+export default Board;
